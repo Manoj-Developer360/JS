aa9b6dad7659103244857c2b950454fed1649a69
Manoj45code <manobala45678@gmail.com>
2025-03-21 13:31:54 +0530

    Added let var const examples

---
+++ let var const/exp2.js
@@ -0,0 +1,12 @@
+const person = { name: "Alice", age: 25 }
+person.age = 30 // Allowed (modifying properties)
+console.log(person.age) 
+console.log(person)
+// person = { name: "Bob" }; // Error (Re-assignment not allowed)
+
+
+const colors = ["red", "blue"]
+colors.push("green") // Allowed
+console.log(colors) 
+
+//colors = ["yellow"]; // Error
